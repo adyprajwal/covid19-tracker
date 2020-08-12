33644b98d9246bf4046d23acbf142e4837c1025d
Prajwal Adhikari <prajwal.ady@gmail.com>
2020-08-12 22:35:03 +0545

    Updates

---
+++ precache-manifest.b378a1ce904b2cdd31b300ea5f3195a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bd5b932475c8065a51ce9037a28d53",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "d38c0c8f5ddfac1834c5",
+    "url": "/covid19-tracker/static/css/main.8a62f59d.chunk.css"
+  },
+  {
+    "revision": "d50ece35ac463a6e6678",
+    "url": "/covid19-tracker/static/js/2.65fdec7f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.65fdec7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38c0c8f5ddfac1834c5",
+    "url": "/covid19-tracker/static/js/main.ca20f721.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
